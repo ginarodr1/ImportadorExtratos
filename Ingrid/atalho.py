e318f313b63754d05f92277ceccddc98a9a35c9a
Regina Rodrigues <gina22rodri@gmail.com>
2025-04-10 11:33:05 -0300

    Códigos apagados no Git e Github.

--- Ingrid/atalho.py
+++
@@ -1,9 +0,0 @@
-#import keyboard
-
-#def acao():
-    #print("Atalho Alt + A detectado!")
-
-#keyboard.add_hotkey("alt+a", acao)
-
-#print("Pressione 'Alt + A' para executar a ação.")
-#keyboard.wait()
