1cce4708d4c0e1f5925568c533c1f339a6412986
Regina Rodrigues <gina22rodri@gmail.com>
2025-05-06 12:41:01 -0300

    (CHORE) Apaguei alguns arquivos utlizados para testes.

--- Judite/backup19032025/setup.py
+++
@@ -1,8 +0,0 @@
-from cx_Freeze import setup, Executable
-
-setup(
-    name="Importador de Extratos Bancários",
-    version="1.0",
-    description="Importador de extratos bancários por meio de .xls e .xlsx, com classificação e exportação.",
-    executables=[Executable("principal.py", base="Win32GUI")],
-)
